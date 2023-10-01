731d5a92f685ef8f5d4994dbf56b67d0cc1837c1
oliviernshima <146647685+oliviernshima@users.noreply.github.com>
2023-10-01 17:34:22 +0200

    Add files via upload

---
+++ assets/custom.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+        		$('.html').animate({width:'98%'},2000);
+        		$('.css').animate({width:'95%'},2000);
+        		$('.bootstrap').animate({width:'97%'},2000);
+            $('.js').animate({width:'70%'},2000);
+            $('.jquery').animate({width:'100%'},2000);
+        		$('.php').animate({width:'100%'},2000);
+        		$('.wp').animate({width:'90%'},2000);
+            $('.angular').animate({width:'80%'},2000);
+            $('.git').animate({width:'95%'},2000);
+            $('.xd').animate({width:'97%'},2000);
+            $('.photoshop').animate({width:'88%'},2000);
+        	});
